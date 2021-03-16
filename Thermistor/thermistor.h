80a376be7e4cf4ec82e09984e3d4bf355028ad1d
Nefeli Metallidou <47782076+nefelimet@users.noreply.github.com>
2021-03-16 15:39:00 +0200

    Create thermistor.h

---
+++ Thermistor/thermistor.h
@@ -0,0 +1,33 @@
+#ifndef THERMISTOR_H
+#define THERMISTOR_H
+
+#include "Arduino.h"
+
+//Connections: resistance R and thermistor connected in series as a voltage divider. Thermistor's voltage taken as input into analog pin "pin"
+
+class Thermistor{
+public:
+  Thermistor(double A, double B, double C, double D, float Rref, float R, byte pin);
+  int takeMeasurement();
+  float measureResistance();
+  float findTemperature();
+  void printData();
+
+private:
+  //Variables A, B, C, D are the thermistor's constants according to the datasheet.
+  double A;
+  double B;
+  double C;
+  double D;
+
+  //Reference resistance, that is the thermistor's nominal resistance, found in the datasheet
+  float Rref;
+
+  //Resistance R connected in series to thermistor
+  float R;
+
+  //Analog input pin
+  byte pin;
+};
+
+#endif
